615940f02fb94e4aed7a01336ffc5da8268f4faa
resq-u <jakub12lepianka@wp.pl>
2025-11-28 13:14:11 +0100

    Add cardClick handler, unique click counter & resetter for imageObjs state

--- src/components/Game/GameContainer.jsx
+++ src/components/Game/GameContainer.jsx
@@ -4,6 +4,7 @@ const url = "https://dog.ceo/api/breeds/image/random/4";
 
 export function GameContainer() {
   const [imageObjs, setImageObjs] = useState([]);
+  // const [highScore, setHighScore] = useState(0);
 
   // move into a separate module?
   useEffect(() => {
@@ -30,6 +31,16 @@ export function GameContainer() {
     };
   }, []);
 
+  function handleCardClick(e){
+    const clickedImgUrl = e.currentTarget.getAttribute("data-url");
+    const clickedImgIndex = imageObjs.findIndex(obj => obj.url === clickedImgUrl)
+    if (!imageObjs[clickedImgIndex].isClicked) {
+      toggleClickStatus(clickedImgIndex);
+    } else {
+      resetImageObjs();
+    }
+  }
+
   function shuffle(array) {
     let arrLength = array.length,
       temp,
@@ -43,18 +54,52 @@ export function GameContainer() {
     return array;
   }
 
+  function countUniqueClicks(){
+    let counter = 0;
+    imageObjs.forEach(obj => obj.isClicked && counter++)
+    return counter;
+  }
+
+  function resetImageObjs() {
+    setImageObjs(prev => {
+      const newArr = [...prev];
+      newArr.forEach(obj => {
+        obj.isClicked = false;
+      });
+
+      return shuffle(newArr);
+    });
+  }
+
+  // function isHighScore(){
+  //   return countUniqueClicks() > highScore;
+  // }
+
+  function toggleClickStatus(index){
+    setImageObjs(prev => {
+        const newArr = shuffle([...prev]);
+        newArr[index] = {
+          ...newArr[index],
+          isClicked: true
+        };
+        return newArr;
+      })
+  }
   // count unique card clicks
   // reset the count when a card has been clicked more than once
+  // things to do onClick
+    // 1. check if clicked obj has been clicked already and change value of isClicked
+      // - if it hasn't, then increase count and shuffle
+      // - if it has, then reset count, reset isClicked values & shuffle
   return (
     <div className="game-container">
+      <h1>Score</h1>
+      <p>Current score: <b>{countUniqueClicks()}</b></p>
+      {/* <p>Best score: <b>{highScore}</b></p> */}
       {imageObjs.length && (
         <CardsContainer
           imageObjs={imageObjs}
-          onCardClick={() =>
-            setImageObjs((prev) => {
-              return shuffle([...prev]);
-            })
-          }
+          onCardClick={handleCardClick}
         ></CardsContainer>
       )}
     </div>
