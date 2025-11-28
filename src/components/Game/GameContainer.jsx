a3c9991b37219d001fc335dd5ce562128c0a1824
resq-u <jakub12lepianka@wp.pl>
2025-11-28 15:00:02 +0100

    Add state based highscore feature

--- src/components/Game/GameContainer.jsx
+++ src/components/Game/GameContainer.jsx
@@ -4,7 +4,8 @@ const url = "https://dog.ceo/api/breeds/image/random/4";
 
 export function GameContainer() {
   const [imageObjs, setImageObjs] = useState([]);
-  // const [highScore, setHighScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+  const currentScore = countUniqueClicks();
 
   // move into a separate module?
   useEffect(() => {
@@ -15,15 +16,15 @@ export function GameContainer() {
       .then((data) => {
         if (!ignore)
           setImageObjs(() => {
-            const imgList = [];
+            const imageList = [];
             data.message.forEach((url, index) => {
-              imgList.push({
+              imageList.push({
                 isClicked: false,
                 order: index,
                 url: url,
               });
             });
-            return imgList;
+            return imageList;
           });
       });
     return () => {
@@ -31,9 +32,11 @@ export function GameContainer() {
     };
   }, []);
 
-  function handleCardClick(e){
+  function handleCardClick(e) {
     const clickedImgUrl = e.currentTarget.getAttribute("data-url");
-    const clickedImgIndex = imageObjs.findIndex(obj => obj.url === clickedImgUrl)
+    const clickedImgIndex = imageObjs.findIndex(
+      (obj) => obj.url === clickedImgUrl
+    );
     if (!imageObjs[clickedImgIndex].isClicked) {
       toggleClickStatus(clickedImgIndex);
     } else {
@@ -54,16 +57,16 @@ export function GameContainer() {
     return array;
   }
 
-  function countUniqueClicks(){
+  function countUniqueClicks() {
     let counter = 0;
-    imageObjs.forEach(obj => obj.isClicked && counter++)
+    imageObjs.forEach((obj) => obj.isClicked && counter++);
     return counter;
   }
 
   function resetImageObjs() {
-    setImageObjs(prev => {
+    setImageObjs((prev) => {
       const newArr = [...prev];
-      newArr.forEach(obj => {
+      newArr.forEach((obj) => {
         obj.isClicked = false;
       });
 
@@ -71,31 +74,31 @@ export function GameContainer() {
     });
   }
 
-  // function isHighScore(){
-  //   return countUniqueClicks() > highScore;
-  // }
-
-  function toggleClickStatus(index){
-    setImageObjs(prev => {
-        const newArr = shuffle([...prev]);
-        newArr[index] = {
-          ...newArr[index],
-          isClicked: true
-        };
-        return newArr;
-      })
+  function isHighScore() {
+    return countUniqueClicks() > highScore;
   }
-  // count unique card clicks
-  // reset the count when a card has been clicked more than once
-  // things to do onClick
-    // 1. check if clicked obj has been clicked already and change value of isClicked
-      // - if it hasn't, then increase count and shuffle
-      // - if it has, then reset count, reset isClicked values & shuffle
+
+  function toggleClickStatus(index) {
+    setImageObjs((prev) => {
+      const newArr = shuffle([...prev]);
+      newArr[index] = {
+        ...newArr[index],
+        isClicked: true,
+      };
+      return newArr;
+    });
+  }
+
   return (
     <div className="game-container">
       <h1>Score</h1>
-      <p>Current score: <b>{countUniqueClicks()}</b></p>
-      {/* <p>Best score: <b>{highScore}</b></p> */}
+      <p>
+        Current score: <b>{currentScore}</b>
+      </p>
+      <p>
+        High score:{" "}
+        <b>{isHighScore() ? setHighScore(currentScore) : highScore}</b>
+      </p>
       {imageObjs.length && (
         <CardsContainer
           imageObjs={imageObjs}
