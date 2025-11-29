c1af703336723a187580d86b92c08d3dfad5ddac
resq-u <jakub12lepianka@wp.pl>
2025-11-29 12:41:20 +0100

    Extract stateSetters for specific APIs, add provisional fetch guard

--- src/components/hooks/useImageFetcher.js
+++ src/components/hooks/useImageFetcher.js
@@ -2,25 +2,47 @@ import { useState, useEffect } from "react";
 
 export function useImageFetcher(url) {
   const [imageObjs, setImageObjs] = useState([]);
+
+  function dogSetter(data) {
+    const arr = [];
+    data.message.forEach((url, index) => {
+      arr.push({
+        isClicked: false,
+        order: index,
+        url: url,
+      });
+    });
+    return arr;
+  }
+
+  function catSetter(data) {
+    const arr = [];
+    data.forEach((obj, index) => {
+      arr.push({
+        isClicked: false,
+        order: index,
+        url: obj.url,
+      });
+    });
+    return arr;
+  }
+
   useEffect(() => {
     let ignore = false;
 
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        if (!ignore)
-          setImageObjs(() => {
-            const imageList = [];
-            data.message.forEach((url, index) => {
-              imageList.push({
-                isClicked: false,
-                order: index,
-                url: url,
-              });
-            });
-            return imageList;
-          });
-      });
+    if (url.includes("cat") || url.includes("dog")) {
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          if (!ignore) {
+            const setter = url.includes("cat")
+              ? catSetter(data)
+              : dogSetter(data);
+            setImageObjs(setter);
+          }
+        });
+    }
+
     return () => {
       ignore = true;
     };
