37126cfe32920231123b341b5e3029b3a10709da
resq-u <jakub12lepianka@wp.pl>
2025-12-01 14:20:23 +0100

    Add font-face, colors, media queries & import the .css files to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/index.css'
-import 'normalize.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/index.css";
+import "normalize.css";
+import "./styles/animations.css"
+import App from "./App.jsx";
+import "./styles/mediaQueries.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
